860b150142e2193a3e3505077ba3e13e07fa0517
andresvicentehector <andresvicentehector@gmail.com>
2023-10-22 19:51:34 +0200

    uploading

--- NumeroMayorTodos.py
+++ NumeroMayorTodos.py
@@ -1,3 +1,23 @@
 ''' EL MAYOR DE CUATRO:
 Crea un script que pida al usuario 4 números diferentes y imprima el mayor de los cuatro por
 pantalla. '''
+
+numero=[]
+for i in range (1,5) :
+    escribe='escribe el numero '+str(i)+':'
+    numero.append(float(input(escribe)))
+# --- Pedir NUMEROS al usuario
+
+
+
+# --- Imprimir el mayor de los cuatro numeros
+if (numero[0]>numero[1]):
+    #numero[0], numero[1] = numero[1], numero[0]
+    numero[1]=numero[0]
+if (numero[1]>numero[2]):
+ # numero[1], numero[2] = numero[2], numero[1]
+  numero[2]=numero[1]
+if (numero[2]>numero[3]):
+     #numero[2], numero[3] = numero[3], numero[2]
+     numero[3]=numero[2]
+print(" El mayor de los 4 es ",   numero[3])
